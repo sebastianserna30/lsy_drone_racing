298e67f7f7eee32ffcd918efe4ecf5a0a4f7debe
Sebastian Serna <sebastian.serna30@gmail.com>
2026-06-10 10:18:23 +0200

    evaluate mppi

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 def main():
     """Run the simulation N times and save the results as 'evaluation.csv'."""
     n_runs = 20
-    config_file = "level2.toml"
+    config_file = "level0_MPPI.toml"
     config = load_config(Path(__file__).parents[1] / "config" / config_file)
     ep_times = simulate(
         config=config_file, controller=config.controller.file, n_runs=n_runs, render=False
